99eb5adf016dd93a3946c1bde543e3b1d13c69b8
Sudarsan-xc <sudarsansharma2020@gmail.com>
2024-07-16 08:01:02 +0545

    starting new

--- modules/activity/activity.controller.js
+++ modules/activity/activity.controller.js
@@ -1,23 +1,39 @@
-const Model = require("./activity.model");
-//crud
-const create = async (payload) => {
-  return await Model.create(payload);
-};
-//read all data
-const list = async () => {
-  return await Model.find();
-};
-//read 1 data
-const getById = async (id) => {
-  return await Model.findOne({ _id: id });
-};
-
 const updateById = async (id, payload) => {
-  return await Model.findOneAndUpdate({ _id: id }, payload, { new: true });
-};
+  try {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      throw new Error("Invalid ID format");
+    }
 
-const removeById = async (id) => {
-  return await Model.deleteOne({ _id: id });
-};
+    // Fetch the activity and its subactivities
+    const activity = await Model.findById(id).populate("subactivities");
 
-module.exports = { create, list, getById, updateById, removeById };
+    if (!activity) {
+      throw new Error("Activity not found");
+    }
+
+    // If marking activity as completed, mark all subactivities as completed
+    if (payload.isCompleted) {
+      await SubActivityModel.updateMany(
+        { activity: id },
+        { isCompleted: true }
+      );
+    } else {
+      // Ensure the activity is not marked as completed if any subactivity is incomplete
+      if (activity.subactivities && activity.subactivities.length > 0) {
+        const incompleteSubactivities = activity.subactivities.filter(
+          (sub) => !sub.isCompleted
+        );
+        if (incompleteSubactivities.length > 0) {
+          throw new Error(
+            "Cannot mark activity as completed. Some subactivities are not completed."
+          );
+        }
+      }
+    }
+
+    // Update the activity
+    return await Model.findOneAndUpdate({ _id: id }, payload, { new: true });
+  } catch (error) {
+    throw new Error(`Error updating activity by ID: ${error.message}`);
+  }
+};
--- modules/subActivity/subActivity.controller.js
+++ modules/subActivity/subActivity.controller.js
@@ -1,23 +1,30 @@
-const Model = require("./subActivity.controller");
-//crud
-const create = async (payload) => {
-  return await Model.create(payload);
-};
-//read all data
-const list = async () => {
-  return await Model.find();
-};
-//read 1 data
-const getById = async (id) => {
-  return await Model.findOne({ _id: id });
-};
-
 const updateById = async (id, payload) => {
-  return await Model.findOneAndUpdate({ _id: id }, payload, { new: true });
-};
+  try {
+    // Update the subactivity
+    const subactivity = await Model.findOneAndUpdate({ _id: id }, payload, {
+      new: true,
+    });
 
-const removeById = async (id) => {
-  return await Model.deleteOne({ _id: id });
-};
+    if (!subactivity) {
+      throw new Error("Subactivity not found");
+    }
 
-module.exports = { create, list, getById, updateById, removeById };
+    // Get the parent activity
+    const activityId = subactivity.activity;
+    const subactivities = await Model.find({ activity: activityId });
+
+    // Check the completion status of all subactivities
+    const allCompleted = subactivities.every((sub) => sub.isCompleted);
+    const activityStatus = allCompleted;
+
+    // Update the parent activity's completion status
+    await activityModel.findOneAndUpdate(
+      { _id: activityId },
+      { isCompleted: activityStatus }
+    );
+
+    return subactivity;
+  } catch (error) {
+    throw new Error(`Error updating subactivity by ID: ${error.message}`);
+  }
+};
